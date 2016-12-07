4daa462bfcf88d8b16f47aacb6d74e6b5652fc68
Nacho <ncd793@gmail.com>
2016-12-07 16:53:10 +0100

    init commit

--- emasa-cliente/src/java/bean/AvisoBean.java
+++ emasa-cliente/src/java/bean/AvisoBean.java
@@ -8,11 +8,13 @@ package bean;
 
 import avisows.AvisoWS_Service;
 import avisows.Aviso;
+import operacionws.Operacion;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
 import javax.xml.ws.WebServiceRef;
+import operacionws.OperacionWS_Service;
 
 /**
  *
@@ -22,18 +24,30 @@ import javax.xml.ws.WebServiceRef;
 @SessionScoped
 public class AvisoBean implements Serializable {
 
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/Emasa-Soap-war/OperacionWS.wsdl")
+    private OperacionWS_Service service_1;
+
     @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/Emasa-Soap-war/AvisoWS.wsdl")
     private AvisoWS_Service service;
    
-    avisows.Aviso avisoSeleccionado; 
+    Aviso avisoSeleccionado; 
     String emailUsuario;
-    List<avisows.Aviso> listaAvisosUsuario;
+    List<Aviso> listaAvisosUsuario;
+    List<Operacion> listaOperaciones;
     /**
      * Creates a new instance of AvisoBean
      */
     public AvisoBean() {
     }
 
+    public List<Operacion> getListaOperaciones(){
+        return listaOperaciones;
+    }
+    
+    public void setListaOperaciones(List<Operacion> lista){
+        this.listaOperaciones = lista;
+    }
+    
     public String getEmailUsuario() {
         return emailUsuario;
     }
@@ -51,7 +65,7 @@ public class AvisoBean implements Serializable {
         return "mostrarAvisos";
     }
 
-    private java.util.List<avisows.Aviso> findAvisoPorUsuario(java.lang.String s) {
+    private java.util.List<Aviso> findAvisoPorUsuario(java.lang.String s) {
         // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
         // If the calling of port operations may lead to race condition some synchronization is required.
         avisows.AvisoWS port = service.getAvisoWSPort();
@@ -73,5 +87,21 @@ public class AvisoBean implements Serializable {
 
     public void setListaAvisosUsuario(List<Aviso> listaAvisosUsuario) {
         this.listaAvisosUsuario = listaAvisosUsuario;
+    }
+    public List<Operacion> getListaOperacionesAviso(){
+        return this.findListaOperaciones(avisoSeleccionado.getId());
+    }
+
+    public String verAviso(Aviso aviso){
+        avisoSeleccionado = aviso;
+        listaOperaciones = getListaOperacionesAviso();
+        return "detalleAviso";
+    }
+    
+    private java.util.List<operacionws.Operacion> findListaOperaciones(int id) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        operacionws.OperacionWS port = service_1.getOperacionWSPort();
+        return port.findListaOperaciones(id);
     }
 }
